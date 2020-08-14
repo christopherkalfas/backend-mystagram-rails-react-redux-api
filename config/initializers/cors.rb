ada759218c1306e9fc345b67df52d02d76c1ef51
christopherkalfas <christopherkalfas22@gmail.com>
2020-08-14 14:08:41 -0400

    fixed cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,7 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
   allow do
-    origins 'mystagram-railsapi-react-redux.herokuapp.com'
+    origins 'https://mystagram-api.herokuapp.com/'
 
     resource '*',
       headers: :any,
